393f8aa2ac34a9d3220fa666f06ab4ae25dc8f21
orangutann <tshimollo9909@gmail.com>
2023-08-10 15:24:22 -0700

    added file 3 4 and 5

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Start
+ *
+ * Description: 'Program to print a string to the console'
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("Programming is like building a multilingual puzzle,");
+return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Start
+ *
+ * Description: 'Program to print a string to the console using printf'
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
