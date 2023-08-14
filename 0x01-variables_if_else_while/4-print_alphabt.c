2b7b78711d90ee43d2e125c6f49134c93425fa01
orangutann <tshimollo9909@gmail.com>
2023-08-14 14:24:19 -0700

    submitted file 5

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		if (letter != 'q' && letter != 'e')
+		{
+			putchar(letter);
+		}
+		++letter;
+	}
+	putchar('\n');
+	return (0);
+}
