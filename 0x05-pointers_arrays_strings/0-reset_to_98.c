d6601921a84762b8586f2c97b17c043c7e14f6c9
orangutann <tshimollo9909@gmail.com>
2023-08-21 18:55:57 -0700

    multifile submission

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - takes pointer to an int and then update the value it points to to 98
+ * @n: input
+ * Return: n
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
