593d7fccf01fca098ed1a38d23dfd20a1c7eb637
orangutann <tshimollo9909@gmail.com>
2023-08-22 17:03:35 -0700

    submission : multiple project file submission

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * reset_to_98 - takes pointer to an int and then update the value it points to to 98
+ * reset_to_98 - takes pointer to an int then update the val it points to to 98
  * @n: input
  * Return: n
  */
@@ -8,4 +8,3 @@ void reset_to_98(int *n)
 {
 	*n = 98;
 }
-
