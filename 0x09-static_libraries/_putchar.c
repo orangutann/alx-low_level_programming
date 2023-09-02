cd371c719e15ac000f0d2da9733ebaca643e7954
orangutann <tshimollo9909@gmail.com>
2023-09-01 19:15:06 -0700

    Sub : File 2 submission

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,22 +1,13 @@
-#include <stdlib.h>
-#include <time.h>
-#include "main.h"
-/* more headers goes there */
-
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	int i = 0;
-	char str[] = "_putchar\n";
-
-	while (str[i] != '\0')
-	{
-		_putchar(str[i]);
-		i++;
-	}
-	return (0);
-}
+#include <unistd.h> 
+ 
+/** 
+ * _putchar - writes the character c to stdout 
+ * @c: The character to print 
+ * 
+ * Return: On success 1. 
+ * On error, -1 is returned, and errno is set appropriately. 
+ */ 
+int _putchar(char c) 
+{ 
+ return (write(1, &c, 1)); 
+} 
